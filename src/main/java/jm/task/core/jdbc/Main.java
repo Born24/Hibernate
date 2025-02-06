0d1181bbcb7807383ef5fb35cc39e0ce6953ca4c
Born24 <106234496+Born24@users.noreply.github.com>
2025-02-06 21:27:45 +0300

    обнова

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -19,27 +19,33 @@ public class Main {
 
             userDao.createUsersTable();
             System.out.println("Таблица 'users' успешно создана (или уже существует).");
+            System.out.println();
 
             userDao.saveUser("Name1", "LastName1", (byte) 20);
             userDao.saveUser("Name2", "LastName2", (byte) 25);
             userDao.saveUser("Name3", "LastName3", (byte) 31);
             userDao.saveUser("Name4", "LastName4", (byte) 38);
             System.out.println("Пользователи успешно добавлены.");
+            System.out.println();
 
             userDao.removeUserById(1);
             System.out.println("Пользователь с ID=1 успешно удалён.");
+            System.out.println();
 
             List<User> users = userDao.getAllUsers();
             System.out.println("Список всех пользователей:");
+            System.out.println();
             for (User user : users) {
                 System.out.println(user);
             }
 
             userDao.cleanUsersTable();
             System.out.println("Таблица 'users' успешно очищена.");
+            System.out.println();
 
             userDao.dropUsersTable();
             System.out.println("Таблица 'users' успешно удалена.");
+            System.out.println();
 
         } catch (Exception e) {
             System.out.println("Ошибка при работе с базой данных: " + e.getMessage());
